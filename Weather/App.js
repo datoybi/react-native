abc610d6803c8e50b41166af75687cde353af798
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-01-05 12:09:18 +0900

    style: change color

--- Weather/App.js
+++ Weather/App.js
@@ -3,10 +3,11 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    <View style={{ flex: 1, backgroundColor: "#fff", alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.container}>
+      {/* <View style={{ flex: 1, backgroundColor: "#fff", alignItems: "center", justifyContent: "center" }}> */}
       <Text style={styles.text}>안녕🤭</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -14,12 +15,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "tomato",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
     fontSize: 28,
-    color: "red",
+    color: "blue",
   },
 });
