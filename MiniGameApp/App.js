17c071921e8a95b0eed769f25f70ab65d20c1da4
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-01-08 10:13:52 +0900

    :art: style: styling box shadow

--- MiniGameApp/App.js
+++ MiniGameApp/App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <StartGameScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ MiniGameApp/screens/StartGameScreen.js
@@ -0,0 +1,34 @@
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    // flex: 1,
+    marginHorizontal: 24,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+
+    // boxShadow - android
+    elevation: 4,
+
+    // boxShadow - ios
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
