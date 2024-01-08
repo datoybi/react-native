8234ea6ed3691140140705a7123388067c1a0b49
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-01-08 10:29:41 +0900

    :gift: feat: keyboard 숫자만 표출 및 대문자 제거 기능 추가

--- MiniGameApp/screens/StartGameScreen.js
+++ MiniGameApp/screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -20,6 +26,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     padding: 16,
     backgroundColor: "#72063c",
+    // backgroundColor: "tomato",
     borderRadius: 8,
 
     // boxShadow - android
@@ -30,5 +37,17 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb62f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
