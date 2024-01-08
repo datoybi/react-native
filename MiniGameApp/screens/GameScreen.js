445c66e7b3469b9579e34a2518e7c88f7bb08906
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-01-08 13:54:46 +0900

    :bug: fix: 난수 에러 잡는중

--- MiniGameApp/screens/GameScreen.js
+++ MiniGameApp/screens/GameScreen.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function generateRandomBetween(min, max, exclude) {
   const randomNumber = Math.floor(Math.random() * (max - min)) + min;
@@ -13,9 +14,22 @@ function generateRandomBetween(min, max, exclude) {
   }
 }
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 function GameScreen({ userNumber }) {
   const initialGuess = generateRandomBetween(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    // direction - lower , grater
+    // if (direction === "lower" && currentGuess < userNumber) || (direction === "greater" && currentGuess > userNumber) {Alert.alert('틀 린수', return;}
+    if (direction === "lower") maxBoundary = currentGuess;
+    else minBoundary = currentGuess + 1;
+
+    const newRandomNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+    setCurrentGuess(newRandomNumber);
+  }
 
   return (
     <View style={styles.screen}>
@@ -24,7 +38,10 @@ function GameScreen({ userNumber }) {
       <Text>{currentGuess}</Text>
       <View>
         <Text>Higher or lower?</Text>
-        {/* + - */}
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>+</PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>-</PrimaryButton>
+        </View>
       </View>
       {/* <View>LOG ROUNDS</View> */}
     </View>
