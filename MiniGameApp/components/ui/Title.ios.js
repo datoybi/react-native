9925508caf55bb3746ff2453c30db7e37b201b91
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-01-12 10:35:46 +0900

    :gift: feat: 플랫폼별 스타일링

---
+++ MiniGameApp/components/ui/Title.ios.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text } from "react-native";
+
+function Title({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "pretendard",
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    borderWidth: 0,
+    borderColor: "white",
+    padding: 12,
+    maxWidth: "80%",
+    width: 300,
+  },
+});
