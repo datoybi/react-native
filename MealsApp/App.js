b40612106878978f59b096c819adc20657a2b947
datoybi <dsy0302@gmail.com>
2024-01-15 20:59:03 +0900

    :gift: feat: 동적 제목 헤더 구현

--- MealsApp/App.js
+++ MealsApp/App.js
@@ -27,7 +27,14 @@ export default function App() {
               title: "All Categories",
             }}
           />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            // options={({ route, navigation }) => {
+            //   const catId = route.params.categoryId;
+            //   return { title: catId };
+            // }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- MealsApp/screens/MealsOverviewScreen.js
+++ MealsApp/screens/MealsOverviewScreen.js
@@ -1,13 +1,19 @@
+import { useLayoutEffect } from "react";
 import { View, StyleSheet, FlatList, Text } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 
-export default function MealsOverviewScreen({ route }) {
+export default function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
+    navigation.setOptions({ title: categoryTitle }); // 제목 setting
+  }, [catId, navigation]);
 
   function renderMealItem(itemData) {
     const item = itemData.item;
