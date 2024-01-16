04d9d0cdbfbfd5fe474ad9f1568926468398e763
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-01-16 09:40:13 +0900

    :gift: feat: drawer navigation 구현

---
+++ DrawerExample/App.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+import "react-native-gesture-handler";
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initalRouteName="User">
+        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ DrawerExample/screens/UserScreen.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function UserScreen() {
+  return <Text>UserScreen</Text>;
+}
