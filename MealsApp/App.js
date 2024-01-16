967dee7ee781ef4208b4ee7e036ecfd9cfdf8a2a
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-01-16 10:31:52 +0900

    :gift: feat: stack과 drawer 네비게이션 중첩으로 사용

--- MealsApp/App.js
+++ MealsApp/App.js
@@ -1,3 +1,5 @@
+import "react-native-gesture-handler";
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 import CategoriesScreen from "./screens/CategoriresScreen";
@@ -5,8 +7,30 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{ title: "All Categories" }}
+      />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -22,10 +46,11 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
               title: "All Categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen
@@ -39,6 +64,7 @@ export default function App() {
           <Stack.Screen
             name="MealDetail"
             component={MealDetailScreen}
+            options={{ title: "About the meal" }}
             // options={{
             //   headerRight: () => {
             //     return <Button title="tap me!" onPress={}/>;
---
+++ MealsApp/screens/FavoritesScreen.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function FavoritesScreen() {
+  return <Text>fav screen</Text>;
+}
