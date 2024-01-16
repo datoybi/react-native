0c71e4a9c23896b44e6a5c7569c1b8f9846f1b76
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-01-16 09:54:31 +0900

    :gift: feat: drawer 세부 사항 구현

--- DrawerExample/App.js
+++ DrawerExample/App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
+import { Ionicons } from "@expo/vector-icons";
+
 import "react-native-gesture-handler";
 
 const Drawer = createDrawerNavigator();
@@ -10,9 +12,34 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initalRouteName="User">
-        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-        <Drawer.Screen name="User" component={UserScreen} />
+      <Drawer.Navigator
+        initalRouteName="User"
+        screenOptions={{
+          headerTintColor: "white",
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          drawerActiveBackgroundColor: "#f0e1ff",
+          drawerActiveTintColor: "#3c0a6b",
+        }}
+      >
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Welcome Screen",
+            drawerIcon: ({ color, size }) => <Ionicons name="home" color={color} size={18} />,
+            // headerTintColor: "white",
+            // drawerActiveBackgroundColor: "#f0e1ff",
+            // drawerActiveTintColor: "#3c0a6b",
+            // drawerStyle: { backgroundColor: "#eaeaea" },
+          }}
+        />
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerIcon: ({ color, size }) => <Ionicons name="person" color={color} size={size} />,
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- DrawerExample/screens/UserScreen.js
+++ DrawerExample/screens/UserScreen.js
@@ -1,5 +1,14 @@
-import { Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-export default function UserScreen() {
-  return <Text>UserScreen</Text>;
+export default function UserScreen({ navigation }) {
+  function openDrawerHandler() {
+    navigation.toggleDrawer(); // drawer 버튼으로 여는 방법
+  }
+
+  return (
+    <View>
+      <Text>UserScreen</Text>
+      <Button title="open Drawer" onPress={openDrawerHandler} />
+    </View>
+  );
 }
