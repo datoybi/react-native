9ce79393f9ec0890151f967eaae95e7507550f1b
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-03-12 10:17:17 +0900

    :gift: feat: redux setting

--- FoodDeliveryApp/App.tsx
+++ FoodDeliveryApp/App.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
-import {useState} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Settings from './src/pages/Settings';
-import Orders from './src/pages/Orders';
-import Delivery from './src/pages/Delivery';
-import SignIn from './src/pages/SignIn';
-import SignUp from './src/pages/SignUp';
+import {Provider} from 'react-redux';
+import store from './src/store';
+import AppInner from './src/AppInner';
 
 export type LoggedInParamList = {
   Orders: undefined;
@@ -21,46 +15,11 @@ export type RootStackParamList = {
   SignUp: undefined;
 };
 
-const Tab = createBottomTabNavigator();
-const Stack = createNativeStackNavigator<RootStackParamList>();
-
 function App() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
   return (
-    <NavigationContainer>
-      {isLoggedIn ? (
-        <Tab.Navigator>
-          <Tab.Screen
-            name="Orders"
-            component={Orders}
-            options={{title: '오더 목록'}}
-          />
-          <Tab.Screen
-            name="Delivery"
-            component={Delivery}
-            options={{headerShown: false}}
-          />
-          <Tab.Screen
-            name="Settings"
-            component={Settings}
-            options={{title: '내 정보'}}
-          />
-        </Tab.Navigator>
-      ) : (
-        <Stack.Navigator>
-          <Stack.Screen
-            name="SignIn"
-            component={SignIn}
-            options={{title: '로그인'}}
-          />
-          <Stack.Screen
-            name="SignUp"
-            component={SignUp}
-            options={{title: '회원가입'}}
-          />
-        </Stack.Navigator>
-      )}
-    </NavigationContainer>
+    <Provider store={store}>
+      <AppInner />
+    </Provider>
   );
 }
 
