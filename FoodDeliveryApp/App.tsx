57a511800ec3716b48a2a132e84b865e9905fa77
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-05-08 18:52:11 +0900

    :bug: fix: ios 실행시 에러 수정

--- FoodDeliveryApp/App.tsx
+++ FoodDeliveryApp/App.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import {Provider} from 'react-redux';
-import store from './src/store';
-import AppInner from './AppInner';
-import {NavigationContainer} from '@react-navigation/native';
+import * as React from "react";
+import { Provider } from "react-redux";
+import store from "./src/store";
+import AppInner from "./AppInner";
+import { NavigationContainer } from "@react-navigation/native";
 
 function App() {
   return (
