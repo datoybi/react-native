b69073fcc0aaa29857b5f8431c51b34a4939ad4a
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-05-08 11:10:34 +0900

    :gift: feat: 앱 이름 변경

--- FoodDeliveryApp/android/app/src/main/java/datoybi/fooddeliveryapp/MainActivity.java
+++ FoodDeliveryApp/android/app/src/main/java/datoybi/fooddeliveryapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.fooddeliveryapp;
+package com.datoybi.fooddeliveryapp;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
--- FoodDeliveryApp/android/app/src/main/java/datoybi/fooddeliveryapp/MainApplication.java
+++ FoodDeliveryApp/android/app/src/main/java/datoybi/fooddeliveryapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.fooddeliveryapp;
+package com.datoybi.fooddeliveryapp;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.fooddeliveryapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.datoybi.fooddeliveryapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
